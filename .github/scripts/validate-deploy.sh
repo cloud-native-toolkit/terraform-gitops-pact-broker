a718b489372b88fc4a45db59ceddbbcff76f9bab
Sean Sundberg <seansund@us.ibm.com>
2021-07-14 12:11:32 -0500

    Updates validate logic
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -3,16 +3,13 @@
 GIT_REPO=$(cat git_repo)
 GIT_TOKEN=$(cat git_token)
 
-KUBECONFIG=$(cat .kubeconfig)
-export KUBECONFIG
-
 mkdir -p .testrepo
 
 git clone https://${GIT_TOKEN}@${GIT_REPO} .testrepo
 
 cd .testrepo || exit 1
 
-ls -l
+find . -name "*"
 
 if [[ ! -f "argocd/2-services/active/pact-broker.yaml" ]]; then
   echo "ArgoCD config missing"
