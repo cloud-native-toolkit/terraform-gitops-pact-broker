e7e43071f3128bf4e46232017d42c8c5467300a1
yogendrasri <ysrivastava@in.ibm.com>
2022-05-18 19:55:25 +0530

    Updates route name and removes duplicate route config (#11)
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ locals {
         enabled = false
       }
       route = {
-        enabled = true
+        enabled = false
       }
       database = {
         type = local.database_type
@@ -23,13 +23,13 @@ locals {
       }
     }
     tool-config = {
-      name = "pactbroker"
+      name = "pact-broker"
       privateUrl = local.service_url
     }
     ocp-route       = {
-      nameOverride = "pactbroker"
+      nameOverride = "pact-broker"
       targetPort = "http"
-      app = "pactbroker"
+      app = "pact-broker"
       serviceName = local.service_name
       termination = "edge"
       insecurePolicy = "Redirect"
