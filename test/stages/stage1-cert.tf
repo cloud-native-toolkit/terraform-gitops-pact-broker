17c5260e2f8a5bc688183e2edf0d7331e1134f46
Ramya Raghuveera <ramragh1@in.ibm.com>
2022-07-13 13:01:43 +0530

    Added seal-secret version in git url
    Signed-off-by: Ramya Raghuveera <ramragh1@in.ibm.com>

--- test/stages/stage1-cert.tf
+++ test/stages/stage1-cert.tf
@@ -1,4 +1,4 @@
 module "cert" {
-  source = "github.com/cloud-native-toolkit/terraform-util-sealed-secret-cert.git"
+  source = "github.com/cloud-native-toolkit/terraform-util-seal-secrets.git?ref=v1.1.0"
 
 }
